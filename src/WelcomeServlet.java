2ea8e61734a9d49e3e027095b7822e4b8c4da453
kana sasazawa <kana.yellow7@gmail.com>
2025-05-09 00:54:34 +0900

    fix 送信ボタンを押した後のエラーを解決

--- src/WelcomeServlet.java
+++ src/WelcomeServlet.java
@@ -17,8 +17,8 @@ public class WelcomeServlet extends HttpServlet {
        
 protected void doPost (HttpServletRequest request,HttpServletResponse response) throws
 ServletException,IOException {
-	request.setCharacterEncoding("UFT-8");
-	response.setContentType("text/html; charset=UFT-8");
+	request.setCharacterEncoding("UTF-8");
+	response.setContentType("text/html; charset=UTF-8");
 	String username = request.getParameter("username");
 	
 	System.out.println(username);
